ac1d56b6b770cedb95a3d7f1ef9b86edaafed4fd
user <user@gmail.com>
2024-11-19 12:39:29 +1100

    Functionality: Can test if you can connect to a port or not

---
+++ port_scanner.py
@@ -0,0 +1,25 @@
+import socket
+
+host = '151.101.192.223'
+port = 80
+
+def test_port(host: str, port: int) -> None:
+    """ Test whether a given port on a host is able to be connected to
+    
+    Args:
+    - host (str): The IPv4 address of the target host
+    - port (int): A valid port number
+
+    Output:
+    - Will Output the result of the connection
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(1)
+        
+        try:
+            sock.connect((host, port))
+            print(f'Connection Succeeded with {host}:{port}')
+        except:
+            print(f'Connection Failed with {host}:{port}')
+
+test_port(host, port)
